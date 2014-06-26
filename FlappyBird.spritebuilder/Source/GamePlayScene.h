f172f204300dae4ca3095943b2472d5b2ff66772
geermc4 <geermc4@gmail.com>
2014-06-25 19:24:09 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -19,7 +19,7 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 {
     // define variables here;
     Character* character;
-    CCPPhysicsNode* physicsNode;
+    CCPhysicsNode* physicsNode;
 }
 
 -(void) initialize;
